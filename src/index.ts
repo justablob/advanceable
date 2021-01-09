a8ea3eced769317e36772688fb528d0f6c250d58
justablob <me@blob.dev>
2021-01-09 16:13:50 +0100

    add write utilities

--- src/index.ts
+++ src/index.ts
@@ -85,6 +85,10 @@ export default class Advanceable {
   readInt40LE() { return this.readIntLE(5) }
   readInt48LE() { return this.readIntLE(6) }
 
+  readString(len: number = 1, encoding?: BufferEncoding) {
+    return this.read(len)?.toString(encoding);
+  }
+
   peek(len: number = 1) {
     if (!this.available(len)) return null;
     return this.buffer.slice(this.offset, this.offset + len);
@@ -96,30 +100,143 @@ export default class Advanceable {
     return true;
   }
 
-  available (len: number = 1) {
+  available(len: number = 1) {
     return (this.offset + len - 1) < this.buffer.length;
   }
 
-  write (data: Buffer)
-  write (data: number[])
-  write (data: string, encoding?: BufferEncoding)
-  write (data: string | Buffer | number[], encoding?: BufferEncoding) {
+  write(data: Buffer)
+  write(data: number[])
+  write(data: string | Buffer | number[]) {
+    if (!this.available(data.length)) return false;
+
     if (Buffer.isBuffer(data)) {
-      if (!this.available(data.length)) return false;
       data.copy(this.buffer, this.offset);
       this.offset += data.length;
-    } else if (typeof data === "string") {
-      let dataLength = Buffer.byteLength(data);
-      if (!this.available(dataLength)) return false;
-      this.buffer.write(data, this.offset, encoding);
-      this.offset += dataLength;
     } else if (Array.isArray(data)) {
-      if (!this.available(data.length)) return false;
       for (let i = 0; i < data.length; i++) {
         this.buffer[this.offset + i] = data[i];
       }
       this.offset += data.length;
     }
+    return true;
+  }
+
+  writeByte(byte: number) {
+    if (!this.available()) return false;
+
+    this.buffer[this.offset] = byte;
+    this.offset++;
+
+    return true;
+  }
+
+  writeUIntBE(len: number, value: number) {
+    if (!this.available(len)) return false;
+
+    this.buffer.writeUIntBE(value, this.offset, len);
+    this.offset += len;
+
+    return true;
+  }
+
+  writeUIntLE(len: number, value: number) {
+    if (!this.available(len)) return false;
+
+    this.buffer.writeUIntLE(value, this.offset, len);
+    this.offset += len;
+
+    return true;
+  }
+
+  writeIntBE(len: number, value: number) {
+    if (!this.available(len)) return false;
+
+    this.buffer.writeIntBE(value, this.offset, len);
+    this.offset += len;
+
+    return true;
+  }
+
+  writeIntLE(len: number, value: number) {
+    if (!this.available(len)) return false;
+
+    this.buffer.writeIntLE(value, this.offset, len);
+    this.offset += len;
+
+    return true;
+  }
+
+  writeBigUInt64BE(value: bigint) {
+    if (!this.available(8)) return false;
+
+    this.writeBigUInt64BE(value);
+    this.offset += 8;
+
+    return true;
+  }
+
+  writeBigUInt64LE(value: bigint) {
+    if (!this.available(8)) return false;
+
+    this.writeBigUInt64LE(value);
+    this.offset += 8;
+
+    return true;
+  }
+
+  writeBigInt64BE(value: bigint) {
+    if (!this.available(8)) return false;
+
+    this.writeBigInt64BE(value);
+    this.offset += 8;
+
+    return true;
+  }
+
+  writeBigInt64LE(value: bigint) {
+    if (!this.available(8)) return false;
+
+    this.writeBigInt64LE(value);
+    this.offset += 8;
+
+    return true;
+  }
+
+  writeUInt8(value: number) { return this.writeUIntBE(1, value) }
+
+  writeUInt16BE(value: number) { return this.writeUIntBE(2, value) }
+  writeUInt24BE(value: number) { return this.writeUIntBE(3, value) }
+  writeUInt32BE(value: number) { return this.writeUIntBE(4, value) }
+  writeUInt40BE(value: number) { return this.writeUIntBE(5, value) }
+  writeUInt48BE(value: number) { return this.writeUIntBE(6, value) }
+
+  writeUInt16LE(value: number) { return this.writeUIntLE(2, value) }
+  writeUInt24LE(value: number) { return this.writeUIntLE(3, value) }
+  writeUInt32LE(value: number) { return this.writeUIntLE(4, value) }
+  writeUInt40LE(value: number) { return this.writeUIntLE(5, value) }
+  writeUInt48LE(value: number) { return this.writeUIntLE(6, value) }
+
+  writeInt8(value: number) { return this.writeIntBE(1, value) }
+
+  writeInt16BE(value: number) { return this.writeIntBE(2, value) }
+  writeInt24BE(value: number) { return this.writeIntBE(3, value) }
+  writeInt32BE(value: number) { return this.writeIntBE(4, value) }
+  writeInt40BE(value: number) { return this.writeIntBE(5, value) }
+  writeInt48BE(value: number) { return this.writeIntBE(6, value) }
+
+  writeInt16LE(value: number) { return this.writeIntLE(2, value) }
+  writeInt24LE(value: number) { return this.writeIntLE(3, value) }
+  writeInt32LE(value: number) { return this.writeIntLE(4, value) }
+  writeInt40LE(value: number) { return this.writeIntLE(5, value) }
+  writeInt48LE(value: number) { return this.writeIntLE(6, value) }
+
+  writeString(data: string, encoding?: BufferEncoding) {
+    let dataLength = Buffer.byteLength(data);
+    if (!this.available(dataLength)) return false;
+
+    this.buffer.write(data, this.offset, encoding);
+    this.offset += dataLength;
+
     return true;
   }
 }
